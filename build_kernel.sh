7a70956510b1d505064d8c3470727c763053326e
sunilpaulmathew <sunil.kde@gmail.com>
2017-12-11 15:09:56 +0900

    Re-generating build script Now it is more universal...
    Signed-off-by: sunilpaulmathew <sunil.kde@gmail.com>

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,12 +1,43 @@
 #!/bin/bash
 
-COLOR_RED="\033[0;31m"
-COLOR_GREEN="\033[1;32m"
-COLOR_NEUTRAL="\033[0m"
-
-echo -e $COLOR_GREEN"\n Stock-Kernel Build Script\n"$COLOR_NEUTRAL
 #
-echo -e $COLOR_GREEN"\n (c) sunilpaulmathew@xda-developers.com\n"$COLOR_NEUTRAL
+# SmartPack-Kernel Build Script
+# 
+# Author: sunilpaulmathew <sunil.kde@gmail.com>
+#
+
+#
+# This script is licensed under the terms of the GNU General Public 
+# License version 2, as published # by the Free Software Foundation, 
+# and may be copied, distributed, and modified under those terms.
+#
+
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR # PURPOSE. See the
+# GNU General Public License for more details.
+#
+
+#
+# ***** ***** ***** ..How to use this script… ***** ***** ***** #
+#
+# For those who want to build this kernel using this script…
+#
+# Please note: this script is by-default designed to build only 
+# one variants at a time.
+#
+
+# 1. Properly locate Stock, UBER & Linaro toolchains (Line# 44, 46 & 48)
+# 2. Select the preferred toolchain for building (Line# 50)
+# 3. Select the 'KERNEL_VARIANT' (Line# 56)
+# 4. Open Terminal, ‘cd’ to the Kernel ‘root’ folder and run ‘. build_kernel.sh’
+# 5. The output (anykernel zip) file will be generated in the ‘release’ folder
+# 6. Enjoy your new Kernel
+
+#
+# ***** ***** *Variables to be configured manually* ***** ***** #
+#
 
 # Toolchains
 
@@ -24,6 +55,8 @@ KERNEL_NAME="Stock-Kernel"
 
 KERNEL_VARIANT="kltekor"	# only one variant at a time
 
+KERNEL_DEFCONFIG="Stock_@$KERNEL_VARIANT@_defconfig"
+
 KERNEL_VERSION="v4-Linaro-7.2"   # leave as such, if no specific version tag
 
 KERNEL_DATE="$(date +"%Y%m%d")"
@@ -31,6 +64,14 @@ KERNEL_DATE="$(date +"%Y%m%d")"
 COMPILE_DTB="y"
 
 NUM_CPUS=""   # number of cpu cores used for build (leave empty for auto detection)
+
+#
+# ***** ***** ***** ***** ***THE END*** ***** ***** ***** ***** #
+#
+
+COLOR_RED="\033[0;31m"
+COLOR_GREEN="\033[1;32m"
+COLOR_NEUTRAL="\033[0m"
 
 export ARCH=$ARCHITECTURE
 
@@ -54,123 +95,66 @@ if [ -z "$NUM_CPUS" ]; then
 fi
 
 if [ -z "$KERNEL_VARIANT" ]; then
-	echo -e $COLOR_GREEN"\n Please select the variant to build... KERNEL_VARIANT should not be empty...\n"$COLOR_NEUTRAL
-fi
-
-if [ "klte" == "$KERNEL_VARIANT" ]; then
-	# creating backups
-	cp scripts/mkcompile_h release/
-	cp arch/arm/configs/lineage_klte_defconfig release/
-	# updating kernel name
-	sed -i "s;Stock-Kernel;$KERNEL_NAME-$KERNEL_VARIANT;" scripts/mkcompile_h;
-	# updating kernel version
-	sed -i "s;lineageos;$KERNEL_VERSION;" arch/arm/configs/lineage_klte_defconfig;
-	if [ -e output_eur-$TOOLCHAIN/.config ]; then
-		rm -f output_eur-$TOOLCHAIN/.config
+	echo -e $COLOR_GREEN"\n Please select the variant to build... 'KERNEL_VARIANT' should not be empty...\n"$COLOR_NEUTRAL
+else
+	if [ -e arch/arm/configs/$KERNEL_DEFCONFIG ]; then
+		# creating backups
+		cp scripts/mkcompile_h release/
+		cp arch/arm/configs/$KERNEL_DEFCONFIG release/
+		# updating kernel name
+		sed -i "s;Stock-Kernel;$KERNEL_NAME-$KERNEL_VARIANT;" scripts/mkcompile_h;
+		# updating kernel version
+		sed -i "s;lineageos;$KERNEL_VERSION;" arch/arm/configs/$KERNEL_DEFCONFIG;
+		if [ -e output_eur-$TOOLCHAIN/.config ]; then
+			rm -f output_eur-$TOOLCHAIN/.config
+			if [ -e output_eur-$TOOLCHAIN/arch/arm/boot/zImage ]; then
+				rm -f output_eur-$TOOLCHAIN/arch/arm/boot/zImage
+			fi
+		else
+			mkdir output_eur-$TOOLCHAIN
+		fi
+		make -C $(pwd) O=output_eur-$TOOLCHAIN $KERNEL_DEFCONFIG && make -j$NUM_CPUS -C $(pwd) O=output_eur-$TOOLCHAIN
 		if [ -e output_eur-$TOOLCHAIN/arch/arm/boot/zImage ]; then
-			rm -f output_eur-$TOOLCHAIN/arch/arm/boot/zImage
-		fi
-	else
-		mkdir output_eur-$TOOLCHAIN
-	fi
-	make -C $(pwd) O=output_eur-$TOOLCHAIN lineage_klte_defconfig && make -j$NUM_CPUS -C $(pwd) O=output_eur-$TOOLCHAIN
-	if [ -e output_eur-$TOOLCHAIN/arch/arm/boot/zImage ]; then
-		echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
-		cp output_eur-$TOOLCHAIN/arch/arm/boot/zImage anykernel/
-		# compile dtb if required
-		if [ "y" == "$COMPILE_DTB" ]; then
-			echo -e $COLOR_GREEN"\n compiling device tree blob (dtb)\n"$COLOR_NEUTRAL
-			if [ -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
-				rm -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img
+			echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
+			cp output_eur-$TOOLCHAIN/arch/arm/boot/zImage anykernel/
+			# compile dtb if required
+			if [ "y" == "$COMPILE_DTB" ]; then
+				echo -e $COLOR_GREEN"\n compiling device tree blob (dtb)\n"$COLOR_NEUTRAL
+				if [ -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
+					rm -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img
+				fi
+				chmod 777 tools/dtbToolCM
+				tools/dtbToolCM -2 -o output_eur-$TOOLCHAIN/arch/arm/boot/dt.img -s 2048 -p output_eur-$TOOLCHAIN/scripts/dtc/ output_eur-$TOOLCHAIN/arch/arm/boot/
+				# removing old dtb (if any)
+				if [ -f anykernel/dtb ]; then
+					rm -f anykernel/dtb
+				fi
+				# copying generated dtb to anykernel directory
+				if [ -e output_eur-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
+					mv -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img anykernel/dtb
+				fi
 			fi
-			chmod 777 tools/dtbToolCM
-			tools/dtbToolCM -2 -o output_eur-$TOOLCHAIN/arch/arm/boot/dt.img -s 2048 -p output_eur-$TOOLCHAIN/scripts/dtc/ output_eur-$TOOLCHAIN/arch/arm/boot/
-			# removing old dtb (if any)
+			echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
+			cd anykernel/ && zip -r9 $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
+			echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
+			rm anykernel/zImage && mv anykernel/$KERNEL_NAME* release/
 			if [ -f anykernel/dtb ]; then
 				rm -f anykernel/dtb
 			fi
-			# copying generated dtb to anykernel directory
-			if [ -e output_eur-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
-				mv -f output_eur-$TOOLCHAIN/arch/arm/boot/dt.img anykernel/dtb
-			fi
-		fi
-		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
-		cd anykernel/ && zip -r9 $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
-		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
-		rm anykernel/zImage && mv anykernel/$KERNEL_NAME* release/
-		if [ -f anykernel/dtb ]; then
-			rm -f anykernel/dtb
-		fi
-		# restoring backups
-		mv release/mkcompile_h scripts/
-		mv release/lineage_klte_defconfig arch/arm/configs/
-		echo -e $COLOR_GREEN"\n everything done... please visit "release"...\n"$COLOR_NEUTRAL
-	else
-		if [ -f anykernel/dtb ]; then
-			rm -f anykernel/dtb
-		fi
-		# restoring backups
-		mv release/mkcompile_h scripts/
-		mv release/lineage_klte_defconfig arch/arm/configs/
-		echo -e $COLOR_GREEN"\n Building error... zImage not found...\n"$COLOR_NEUTRAL
-	fi
-fi
-
-if [ "kltekor" == "$KERNEL_VARIANT" ]; then
-	# creating backups
-	cp scripts/mkcompile_h release/
-	cp arch/arm/configs/lineage_kltekor_defconfig release/
-	# updating kernel name
-	sed -i "s;Stock-Kernel;$KERNEL_NAME-$KERNEL_VARIANT;" scripts/mkcompile_h;
-	# updating kernel version
-	sed -i "s;lineageos;$KERNEL_VERSION;" arch/arm/configs/lineage_kltekor_defconfig;
-	if [ -e output_kor-$TOOLCHAIN/.config ]; then
-		rm -f output_kor-$TOOLCHAIN/.config
-		if [ -e output_kor-$TOOLCHAIN/arch/arm/boot/zImage ]; then
-			rm -f output_kor-$TOOLCHAIN/arch/arm/boot/zImage
-		fi
-	else
-		mkdir output_kor-$TOOLCHAIN
-	fi
-	make -C $(pwd) O=output_kor-$TOOLCHAIN lineage_kltekor_defconfig && make -j$NUM_CPUS -C $(pwd) O=output_kor-$TOOLCHAIN
-	if [ -e output_kor-$TOOLCHAIN/arch/arm/boot/zImage ]; then
-		echo -e $COLOR_GREEN"\n copying zImage to anykernel directory\n"$COLOR_NEUTRAL
-		cp output_kor-$TOOLCHAIN/arch/arm/boot/zImage anykernel/
-		# compile dtb if required
-		if [ "y" == "$COMPILE_DTB" ]; then
-			echo -e $COLOR_GREEN"\n compiling device tree blob (dtb)\n"$COLOR_NEUTRAL
-			if [ -f output_kor-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
-				rm -f output_kor-$TOOLCHAIN/arch/arm/boot/dt.img
-			fi
-			chmod 777 tools/dtbToolCM
-			tools/dtbToolCM -2 -o output_kor-$TOOLCHAIN/arch/arm/boot/dt.img -s 2048 -p output_kor-$TOOLCHAIN/scripts/dtc/ output_kor-$TOOLCHAIN/arch/arm/boot/
-			# removing old dtb (if any)
+			# restoring backups
+			mv release/mkcompile_h scripts/
+			mv release/$KERNEL_DEFCONFIG arch/arm/configs/
+			echo -e $COLOR_GREEN"\n everything done... please visit "release"...\n"$COLOR_NEUTRAL
+		else
 			if [ -f anykernel/dtb ]; then
 				rm -f anykernel/dtb
 			fi
-			# copying generated dtb to anykernel directory
-			if [ -e output_kor-$TOOLCHAIN/arch/arm/boot/dt.img ]; then
-				mv -f output_kor-$TOOLCHAIN/arch/arm/boot/dt.img anykernel/dtb
-			fi
+			# restoring backups
+			mv release/mkcompile_h scripts/
+			mv release/$KERNEL_DEFCONFIG arch/arm/configs/
+			echo -e $COLOR_GREEN"\n Building error... zImage not found...\n"$COLOR_NEUTRAL
 		fi
-		echo -e $COLOR_GREEN"\n generating recovery flashable zip file\n"$COLOR_NEUTRAL
-		cd anykernel/ && zip -r9 $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip * -x README.md $KERNEL_NAME-$KERNEL_VARIANT-$KERNEL_VERSION-$KERNEL_DATE.zip && cd ..
-		echo -e $COLOR_GREEN"\n cleaning...\n"$COLOR_NEUTRAL
-		rm anykernel/zImage && mv anykernel/$KERNEL_NAME* release/
-		if [ -f anykernel/dtb ]; then
-			rm -f anykernel/dtb
-		fi
-		# restoring backups
-		mv release/mkcompile_h scripts/
-		mv release/lineage_kltekor_defconfig arch/arm/configs/
-		echo -e $COLOR_GREEN"\n everything done... please visit "release"...\n"$COLOR_NEUTRAL
 	else
-		if [ -f anykernel/dtb ]; then
-			rm -f anykernel/dtb
-		fi
-		# restoring backups
-		mv release/mkcompile_h scripts/
-		mv release/lineage_kltekor_defconfig arch/arm/configs/
-		echo -e $COLOR_GREEN"\n Building error... zImage not found...\n"$COLOR_NEUTRAL
+		echo -e $COLOR_GREEN"\n '$KERNEL_VARIANT' is not a supported variant... please check...\n"$COLOR_NEUTRAL
 	fi
 fi
